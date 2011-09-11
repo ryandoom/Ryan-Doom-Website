1f188c5ea6bcb2fae3a6d11e78904f63fd8e88ef
Ryan Doom <ryan.doom@webascender.com>
2011-09-11 17:41:20 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RyandoomWebsite::Application.config.secret_token = 'f9bb6946599841bffbfafe1680f24f6fe122913461c9e6acf63c2da95d2eea37753b3ce9f74ba2c0aaeb5171953acfe133780c684a69735aa5c3f5591a8cd53b'
